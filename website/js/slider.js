5b46da9950604f77eac776351a3ea33345d13aeb
IgorLucindo <lucindoigor@yahoo.com.br>
2025-02-28 23:52:41 -0600

    updated design

--- website/js/slider.js
+++ website/js/slider.js
@@ -3,24 +3,47 @@ const images = slides.querySelectorAll('img');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
 let index = 0;
+let slider_interval;
 
-function showSlide(index) {
+
+// show slide with index i
+function showSlide(i) {
     const slideWidth = images[0].clientWidth;
-    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+    slides.style.transform = `translateX(-${i * slideWidth}px)`;
 }
 
+
+// create slider interval
+function createSliderInterval() {
+    // 4s interval
+    time = 4000;
+    // create interval
+    slider_interval = setInterval(() => {
+        index = (index + 1) % images.length;
+        showSlide(index);
+    }, time);
+}
+
+
+// reset slider interval
+function resetInterval() {
+    clearInterval(slider_interval);
+    createSliderInterval();
+}
+
+
+// navigation buttons for slider
 nextButton.addEventListener('click', () => {
     index = (index + 1) % images.length;
     showSlide(index);
+    resetInterval();
 });
-
 prevButton.addEventListener('click', () => {
     index = (index - 1 + images.length) % images.length;
     showSlide(index);
+    resetInterval();
 });
 
-// Auto-slide every 3 seconds (optional)
-setInterval(() => {
-    index = (index + 1) % images.length;
-    showSlide(index);
-}, 3000);
+
+// create slider interval
+createSliderInterval();
